e4c9e70054dd9372f378de6cf3a34ecd447797e4
mayegirl <59669195+mayegirl@users.noreply.github.com>
2021-01-23 18:50:15 -0700

    trying

--- js/main.js
+++ js/main.js
@@ -1,8 +1,20 @@
 var notes = ["Week 1", "Week 2", "Week 3"];
+const links = [
+    {
+        label: "Week 1 notes",
+        url: "Week1.html"
+    }
+]
 
 function myFunction{
     var i, len, text;
     for (i = 0, len = notes.length, text = ""; i < len; i++) {
-        text += notes[i] + "<a href = "notes[i].html">" + "<br>";
+      //  text += notes[i] + "<br>";
+        var a = document.createElement("a");
+        var link = document.createTextNode(notes[i] + ".html");
+        a.appendChild(link);
+        a.title = text;
+        a.href = link;
+        document.body.appendChild(a);
     };
 }
