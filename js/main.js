b054f471c9abda9e52adc3be116cdea8f9dac611
mayegirl <59669195+mayegirl@users.noreply.github.com>
2021-01-23 17:47:44 -0700

    test

--- js/main.js
+++ js/main.js
@@ -1,9 +1,15 @@
 var notes = ["Week 1", "Week 2", "Week 3"];
 notes.forEach(myFunction);
+var x = document.getElementById("demo";)
 
 function myFunction(){
-    var i;
-    for (i = 0; i < notes.length; i++) {
         var x = document.createElement("LI") + notes[i] + var y = document.createElement("BR");
-      }
+        
+        
+        
+        const links = [
+            {
+              label: "Week1 notes",
+              url: "week1/index.html"
+            }  
 }
