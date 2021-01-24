6734319ecbd30258886df85e84255ccb025828a6
mayegirl <59669195+mayegirl@users.noreply.github.com>
2021-01-23 18:28:57 -0700

    ty

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 var notes = ["Week 1", "Week 2", "Week 3"];
-var i, len, text;
-for (i = 0, len = notes.length, text = ""; i < len; i++) {
-  text += notes[i] + "<br>";
+
+function myFunction{
+    var i, len, text;
+    for (i = 0, len = notes.length, text = ""; i < len; i++) {
+        text += notes[i] + "<br>";
+    };
 }
-document.getElementById("demo").innerHTML = text;
