8128f729a16297f5e1f281525b24f07b446e9118
mayegirl <59669195+mayegirl@users.noreply.github.com>
2021-01-23 18:17:18 -0700

    typo fix

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 var notes = ["Week 1", "Week 2", "Week 3"];
 var i, len, text;
 for (i = 0, len = notes.length, text = ""; i < len; i++) {
-  text += cars[i] + "<br>";
+  text += notes[i] + "<br>";
 }
 document.getElementById("demo").innerHTML = text;
