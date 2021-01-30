c0dfdb9976d9269688d4d61b19b52b06a2471629
mayegirl <59669195+mayegirl@users.noreply.github.com>
2021-01-29 21:15:36 -0700

    fixed links, added 3

--- js/main.js
+++ js/main.js
@@ -1,5 +1,3 @@
-console.log("Testing javasc");
-
 const notes = [
     {
         label: "Week 1 Notes",
@@ -13,14 +11,19 @@ const notes = [
         label: "Week 3 Notes",
         url: "Week3.html"
     },
+    
 ]
-function myFunction{
-    var i, len, text;
-    for(i = 0; len = notes.length; i++){
-        var li = document.createElement("LI");
+function myFunction(){
+    var i, text;
+    var ol = document.getElementById("demo");
+    for(i = 0; notes.length; i++){
+        var li = document.createElement("li");
         var a = document.createElement("a");
-        a.setAttribute("href", notes[i].url());
-        a.innerHTML = notes[i].label();
-        document.getElementById("demo");
+        a.setAttribute("href", notes[i].url);
+        a.innerHTML = notes[i].label;
+        li.appendChild(a);
+        ol.appendChild(li);
     }
 }
+
+myFunction();
